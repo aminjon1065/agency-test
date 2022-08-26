0c9b2f8df7711bc690440e38d36033d07d345324
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2022-08-26 15:09:07 +0500

    0.0.1

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,15 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [\App\Http\Controllers\Api\Auth\AuthController::class, 'register'])->name('user.register');
 Route::post('/login', [\App\Http\Controllers\Api\Auth\AuthController::class, 'login'])->name('user.login')->name('user.login');
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('/isAuth', [\App\Http\Controllers\Api\Auth\AuthController::class, 'isAuth'])->name('user.isAuth');
-    Route::post('/logout', [\App\Http\Controllers\Api\Auth\AuthController::class, 'logout'])->name('user.logout');
-    Route::post('/upload', [\App\Http\Controllers\Api\Files\FilesController::class, 'upload'])->name('files.upload');
-    Route::get('/files', [\App\Http\Controllers\Api\Files\FilesController::class, 'index'])->name('files.get');
-    Route::post('/message', [\App\Http\Controllers\Api\Messages\MessagesController::class, 'newMessage'])->name('message.new');
-    Route::get('/inbox', [\App\Http\Controllers\Api\Messages\MessagesController::class, 'inbox'])->name('message.inbox');
-    Route::get('/email/verify/{id}', [\App\Http\Controllers\Api\Auth\EmailVerification::class, 'verify']);
-    Route::get('/users-list', [\App\Http\Controllers\Api\Users\GetUsersList::class, 'getUsers'])->name('users.list');
+    Route::prefix('v1')->group(function () {
+        Route::get('/isAuth', [\App\Http\Controllers\Api\Auth\AuthController::class, 'isAuth'])->name('user.isAuth');
+        Route::post('/logout', [\App\Http\Controllers\Api\Auth\AuthController::class, 'logout'])->name('user.logout');
+        Route::post('/upload', [\App\Http\Controllers\Api\Files\FilesController::class, 'upload'])->name('files.upload');
+        Route::get('/files', [\App\Http\Controllers\Api\Files\FilesController::class, 'index'])->name('files.get');
+        Route::post('/message', [\App\Http\Controllers\Api\Messages\MessagesController::class, 'newMessage'])->name('message.new');
+        Route::get('/inbox', [\App\Http\Controllers\Api\Messages\MessagesController::class, 'inbox'])->name('message.inbox');
+        Route::get('/email/verify/{id}', [\App\Http\Controllers\Api\Auth\EmailVerification::class, 'verify']);
+        Route::get('/users-list', [\App\Http\Controllers\Api\Users\GetUsersList::class, 'getUsers'])->name('users.list');
+    });
 //    Route::get('/email/resend', [\App\Http\Controllers\Api\Auth\EmailVerification::class, 'resend']);
 });
