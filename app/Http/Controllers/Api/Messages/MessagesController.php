8e86ff38df25b98c550ad896a78efcd74ea703bb
Aminjon Aliev <aminjon1065@gmail.com>
2022-09-19 11:14:38 +0500

    0.0.1

--- app/Http/Controllers/Api/Messages/MessagesController.php
+++ app/Http/Controllers/Api/Messages/MessagesController.php
@@ -11,6 +11,7 @@ class MessagesController extends Controller
 {
     public function newMessage(Request $request)
     {
+//        return response()->json($request->allFiles());
         $validator = Validator::make($request->all(), [
             'title' => 'required|string|min:4',
             'subject' => 'nullable',
@@ -25,36 +26,49 @@ class MessagesController extends Controller
                 'data' => $validator->messages()
             ], 500);
         }
-
         if ($request->hasFile('files_link')) {
             foreach ($request->file('files_link') as $file) {
                 $name = rand() . '-' . $file->getClientOriginalName();
-                $folderName = date('d-m-Y') . '/email-files/'.auth()->user()->name;
+                $folderName = date('d-m-Y') . '/email-files/' . auth()->user()->name;
                 $file->move(public_path() . '/' . $folderName, $name);
-                $files_links[] = $folderName . '/' . $name;
+                $files_link[] = $folderName . '/' . $name;
             }
+//            return $files_link;
         }
-        $data = [
-            'title' => $request->title,
-            'subject' => $request->subject,
-            'description' => $request->description,
-            'files_link' => $files_links,
-            'opened' => $request->opened,
-            'from' => auth()->id(),
-            'to' => $request->to,
-        ];
+        $toArr[] = $request->to;
+        $toArrs = [...$toArr[0]];
+//        return response()->json($toArrs);
+//        if (!empty($toArr) && isset($toArr) && is_array($toArr)) {
+        foreach ($toArrs as $key => $item) {
+            $sendTo[$key] = $item;
+        }
+//        }
 
-        $message = Messages::create($data);
+        foreach ($sendTo as $key => $item) {
+            $data[$key] = [
+                'title' => $request->title,
+                'subject' => $request->subject,
+                'description' => $request->description,
+                'files_link' => $files_link,
+                'opened' => 0,
+                'from' => auth()->id(),
+                'to' => $item,
+            ];
+            $message = Messages::create($data[$key]);
+        }
+
         return response()->json([
             'success' => true,
-            'data' => $message
+            'data' => "Sended"
         ], 201);
     }
 
 
     public function inbox()
     {
-        $messages = Messages::where('from', auth()->user()->id)->paginate(25);
+        $messages = Messages::where('from', auth()->user()->id)->paginate(40);
+
+//        $messages['image'] = implode(' ', $messages['files_link']);
         return response()->json($messages);
     }
 }
